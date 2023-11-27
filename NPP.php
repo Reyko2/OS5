b45d0843c318715b452868c0886e0046b0162afe
Reyko2 <rico.nieto.cics@ust.edu.ph>
2023-11-27 22:23:39 +0800

    Added Index, NPP, and SRTF php codes

---
+++ NPP.php
@@ -0,0 +1,116 @@
+<?php
+function priorityScheduling($jobs)
+{
+    $n = count($jobs);
+
+      // Check if any burst time is zero
+      foreach ($jobs as $job) {
+        if ($job['arrivalTime'] < 0 || $job['burstTime'] < 0 || $job['priority'] < 0) {
+            echo "Error: Invalid input.";
+            return;
+        }
+    }
+
+// Sort jobs by priority only
+    usort($jobs, function ($a, $b) {
+        return $a['priority'] - $b['priority'];
+    });
+
+
+    $finishTime = array_fill(0, $n, 0);
+    $turnaroundTime = array_fill(0, $n, 0);
+    $waitingTime = array_fill(0, $n, 0);
+
+    $finishTime[0] = $jobs[0]['arrivalTime'] + $jobs[0]['burstTime'];
+    $turnaroundTime[0] = $finishTime[0] - $jobs[0]['arrivalTime'];
+    $waitingTime[0] = $turnaroundTime[0] - $jobs[0]['burstTime'];
+
+    for ($i = 1; $i < $n; $i++) {
+        $finishTime[$i] = $finishTime[$i - 1] + $jobs[$i]['burstTime'];
+        $turnaroundTime[$i] = $finishTime[$i] - $jobs[$i]['arrivalTime'];
+        $waitingTime[$i] = $turnaroundTime[$i] - $jobs[$i]['burstTime'];
+    }
+
+    // Calculate averages
+    $averageTurnaroundTime = array_sum($turnaroundTime) / $n;
+    $averageWaitingTime = array_sum($waitingTime) / $n;
+
+    // Output the table
+    echo "NPP";
+    echo "<table border='1'>";
+    echo "<tr><th>Job</th><th>Arrival Time</th><th>Burst Time</th><th>Finish Time</th><th>Turnaround Time</th><th>Waiting Time</th><th>Priority</th></tr>";
+    for ($i = 0; $i < $n; $i++) {
+        echo "<tr><td>{$jobs[$i]['job']}</td><td>{$jobs[$i]['arrivalTime']}</td><td>{$jobs[$i]['burstTime']}</td><td>{$finishTime[$i]}</td><td>{$turnaroundTime[$i]}</td><td>{$waitingTime[$i]}</td><td>{$jobs[$i]['priority']}</td></tr>";
+    }
+    echo "</table>";
+
+    // Output averages
+    echo "<p>Average Turnaround Time: {$averageTurnaroundTime}</p>";
+    echo "<p>Average Waiting Time: {$averageWaitingTime}</p>";
+}
+
+// Validate and process the form data
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $arrivalTimes = $_POST['arrival_times'];
+    $burstTimes = $_POST['burst_times'];
+    $priorities = $_POST['priorities'];
+
+    // Convert input strings to arrays
+    $arrivalTimes = preg_split("/[\s,]+/", $arrivalTimes);
+    $burstTimes = preg_split("/[\s,]+/", $burstTimes);
+    
+    // If priorities input is empty, assume default values of 1 for all priorities
+    if (empty($priorities)) {
+        $priorities = array_fill(0, count($arrivalTimes), 1);
+    } else {
+        $priorities = preg_split("/[\s,]+/", $priorities);
+    }
+
+
+    // Validate input counts
+    if (count($arrivalTimes) == count($burstTimes) && count($burstTimes) == count($priorities)) {
+        $jobs = [];
+        $n = count($arrivalTimes);
+
+        for ($i = 0; $i < $n; $i++) {
+            $jobs[] = [
+                'job' => chr(65 + $i), // Convert to letters A, B, C
+                'arrivalTime' => intval($arrivalTimes[$i]),
+                'burstTime' => intval($burstTimes[$i]),
+                'priority' => intval($priorities[$i]),
+            ];
+        }
+
+        // Call the scheduling function
+        priorityScheduling($jobs);
+    } else {
+        echo "Error: The number of arrival times, burst times, and priorities must be the same.";
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Priority Scheduling</title>
+</head>
+<body>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <label for="arrival_times">Arrival Times:</label>
+        <input type="text" name="arrival_times" required><br>
+
+        <label for="burst_times">Burst Times:</label>
+        <input type="text" name="burst_times" required><br>
+
+        <label for="priorities">Priorities:</label>
+        <input type="text" name="priorities"><br>
+
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+
---
+++ SRTF.php
@@ -0,0 +1,145 @@
+<?php
+class Process {
+    public $pid;
+    public $arrival_time;
+    public $burst_time;
+    public $start_time;
+    public $completion_time;
+    public $turnaround_time;
+    public $waiting_time;
+    public $response_time;
+ 
+    public function __construct($pid, $arrival_time, $burst_time) {
+        $this->pid = $pid;
+        $this->arrival_time = $arrival_time;
+        $this->burst_time = $burst_time;
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Process the form data
+
+    // Validate the input
+    $arrival_times_input = $_POST["arrival_times"];
+    $burst_times_input = $_POST["burst_times"];
+
+    $arrival_times = array_map('intval', preg_split('/[\s,]+/', $arrival_times_input));
+    $burst_times = array_map('intval', preg_split('/[\s,]+/', $burst_times_input));
+
+    // Validation: Check if burst times start with 0
+    if (in_array(0, $burst_times)) {
+        echo "Error: 0 is invalid burst time.";
+        exit;
+    }
+
+    $num_processes = count($arrival_times);
+
+    if ($num_processes != count($burst_times)) {
+        echo "Error: Number of arrival times and burst times should be the same.";
+        exit;
+    }
+
+$processes = array();
+$burst_remaining = array();
+$total_turnaround_time = 0;
+$total_waiting_time = 0;
+$total_response_time = 0;
+$total_idle_time = 0;
+
+
+for($i = 0; $i < $num_processes; $i++) {
+    $processes[$i] = new Process($i, $arrival_times[$i], $burst_times[$i]);
+    $burst_remaining[$i] = $burst_times[$i];
+}
+
+$current_time = 0;
+$completed = 0;
+
+while ($completed != $num_processes) {
+    $idx = -1;
+    $min_burst = INF;
+
+    // Find the process with the shortest remaining time
+    for ($i = 0; $i < $num_processes; $i++) {
+        if ($processes[$i]->arrival_time <= $current_time && $burst_remaining[$i] < $min_burst && $burst_remaining[$i] > 0) {
+            $min_burst = $burst_remaining[$i];
+            $idx = $i;
+        }
+    }
+
+    if ($idx != -1) {
+        if ($burst_remaining[$idx] == $processes[$idx]->burst_time) {
+            $processes[$idx]->start_time = $current_time;
+            $total_idle_time += $processes[$idx]->start_time - $current_time;
+        }
+
+        $burst_remaining[$idx]--;
+        $current_time++;
+
+        if ($burst_remaining[$idx] == 0) {
+            $processes[$idx]->completion_time = $current_time;
+            $processes[$idx]->turnaround_time = $processes[$idx]->completion_time - $processes[$idx]->arrival_time;
+            $processes[$idx]->waiting_time = $processes[$idx]->turnaround_time - $processes[$idx]->burst_time;
+            $processes[$idx]->response_time = $processes[$idx]->start_time - $processes[$idx]->arrival_time;
+            $total_turnaround_time += $processes[$idx]->turnaround_time;
+            $total_waiting_time += $processes[$idx]->waiting_time;
+            $total_response_time += $processes[$idx]->response_time;
+            $completed++;
+        }
+    } else {
+        $current_time++;
+    }
+}
+
+$avg_turnaround_time = $total_turnaround_time / $num_processes;
+$avg_waiting_time = $total_waiting_time / $num_processes;
+$avg_response_time = $total_response_time / $num_processes;
+
+// Output the results as needed
+echo "SRTF";
+echo "<table border='1'>";
+echo "<tr>";
+echo "<th>Process</th><th>Arrival Time</th><th>Burst Time</th><th>Start Time</th><th>Completion Time</th><th>Turnaround Time</th><th>Waiting Time</th>";
+echo "</tr>";
+
+foreach ($processes as $process) {
+    echo "<tr>";
+    echo "<td>{$process->pid}</td>";
+    echo "<td>{$process->arrival_time}</td>";
+    echo "<td>{$process->burst_time}</td>";
+    echo "<td>{$process->start_time}</td>";
+    echo "<td>{$process->completion_time}</td>";
+    echo "<td>{$process->turnaround_time}</td>";
+    echo "<td>{$process->waiting_time}</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+echo "Average Turnaround Time = " . $avg_turnaround_time . "<br>";
+echo "Average Waiting Time = " . $avg_waiting_time . "<br>";
+
+} 
+else {
+// Display the form
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>SRTF Scheduler</title>
+</head>
+<body>
+<h1>SRTF Scheduler</h1>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <label for="arrival_times">Arrival Times:</label>
+    <input type="text" name="arrival_times" required>
+    <br>
+    <label for="burst_times">Burst Times:</label>
+    <input type="text" name="burst_times" required>
+    <br>
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
+<?php } ?>
