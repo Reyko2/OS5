16a7e2223959da857781e5ac34faaf14227c5949
Reyko2 <rico.nieto.cics@ust.edu.ph>
2023-11-29 16:30:04 +0800

    Update SCAN.php

--- SCAN.php
+++ SCAN.php
@@ -35,29 +35,94 @@
         $requests = array_map('intval', $requests);
 
         // Perform SCAN algorithm calculation
-        $sortedRequests = $requests;
-        sort($sortedRequests);
+        function SCAN($arr, $head, $direction) {
+            $seek_count = 0;
+            $distance = 0;
+            $cur_track = 0;
+            $left = [];
+            $right = [];
+            $seek_sequence = [];
 
-        $currentIndex = array_search($currentPosition, $sortedRequests);
-        $leftRequests = array_slice($sortedRequests, 0, $currentIndex);
-        $rightRequests = array_slice($sortedRequests, $currentIndex + 1);
+            // Appending end values
+            // which has to be visited
+            // before reversing the direction
+            if ($direction == "left") {
+                array_push($left, 0);
+            } elseif ($direction == "right") {
+                array_push($right, $GLOBALS['trackSize'] - 1);
+            }
 
-        $leftHeadMovement = array_sum(array_map(function ($request) use ($currentPosition) {
-            return abs($request - $currentPosition);
-        }, $leftRequests));
+            for ($i = 0; $i < count($arr); $i++) {
+                if ($arr[$i] < $head) {
+                    array_push($left, $arr[$i]);
+                }
+                if ($arr[$i] > $head) {
+                    array_push($right, $arr[$i]);
+                }
+            }
 
-        $rightHeadMovement = array_sum(array_map(function ($request) use ($currentPosition) {
-            return abs($request - $currentPosition);
-        }, $rightRequests));
+            // Sorting left and right vectors
+            sort($left);
+            sort($right);
 
-        $totalHeadMovement = $leftHeadMovement + $rightHeadMovement;
-        $seekTime = $totalHeadMovement / $seekRate;
+            // Run the while loop two times.
+            // One by one scanning right
+            // and left of the head
+            $run = 2;
+            while ($run-- > 0) {
+                if ($direction == "left") {
+                    for ($i = count($left) - 1; $i >= 0; $i--) {
+                        $cur_track = $left[$i];
+
+                        // Appending current track to seek sequence
+                        array_push($seek_sequence, $cur_track);
+
+                        // Calculate absolute distance
+                        $distance = abs($cur_track - $head);
+
+                        // Increase the total count
+                        $seek_count += $distance;
+
+                        // Accessed track is now the new head
+                        $head = $cur_track;
+                    }
+                    $direction = "right";
+                } elseif ($direction == "right") {
+                    for ($i = 0; $i < count($right); $i++) {
+                        $cur_track = $right[$i];
+
+                        // Appending current track to seek sequence
+                        array_push($seek_sequence, $cur_track);
+
+                        // Calculate absolute distance
+                        $distance = abs($cur_track - $head);
+
+                        // Increase the total count
+                        $seek_count += $distance;
+
+                        // Accessed track is now new head
+                        $head = $cur_track;
+                    }
+                    $direction = "left";
+                }
+            }
+
+            // Calculate total head movement and seek time
+            $totalHeadMovement = $seek_count;
+            $seekTime = $totalHeadMovement / $GLOBALS['seekRate'];
+
+            // Display results
+            echo "<p>Total number of seek operations = $seek_count</p>";
+            echo "<p>Total head movement: $totalHeadMovement tracks</p>";
+            echo "<p>Seek time: $seekTime seconds</p>";
+            echo "<p>Seek Sequence is: </p>";
+            for ($i = 0; $i < count($seek_sequence); $i++) {
+                echo "<p>$seek_sequence[$i]</p>";
+            }
+        }
 
         // Display results
-        echo "<div id='result'>";
-        echo "<p>Total head movement: $totalHeadMovement tracks</p>";
-        echo "<p>Seek time: $seekTime seconds</p>";
-        echo "</div>";
+        SCAN($requests, $currentPosition, "left");
     }
     ?>
 </body>
